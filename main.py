310a37328ab4a5e9800888dbe01d222a65007ea4
Ryan A. Walker <rawn8c@mail.umsl.edu>
2017-01-26 21:01:26 -0600

    Implemented structure and basic functionality. Includes error checking for all error cases. TO-DO: 1.) Get error messages to appear next to offending box. Will probably have to rewrite the way the page builds itself to do so. 2.) Preserve user input for username and email fields between failed inputs, but leave the password fields cleared.

--- main.py
+++ main.py
@@ -15,11 +15,120 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
+import re
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+PASS_RE = re.compile(r"^.{3,20}$")
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+
+def valid_username(username):
+    return USER_RE.match(username)
+
+def valid_password(password):
+    return PASS_RE.match(password)
+
+def valid_email(email):
+    return EMAIL_RE.match(email)
+
+
+header = '''
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>User Signup</title>
+    </head>
+    <body>
+'''
+
+footer = '''
+    </body>
+</html>
+'''
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        form_body = '''
+        <h1>Signup</h1>
+        <form method="post">
+            <table>
+                <tr>
+                    <td class="label">
+                        Username
+                    </td>
+                    <td>
+                        <input type="text" name="username" value="">
+                    </td>
+                </tr>
+                <tr>
+                    <td class="label">
+                        Password
+                    </td>
+                    <td>
+                        <input type="password" name="password" value="">
+                    </td>
+                </tr>
+                <tr>
+                    <td class="label">
+                        Verify Password
+                    </td>
+                    <td>
+                        <input type="password" name="verify" value="">
+                    </td>
+                </tr>
+                <tr>
+                    <td class="label">
+                        Email (Optional)
+                    </td>
+                    <td>
+                        <input type="text" name="email" value="">
+                    </td>
+                </tr>
+            </table>
+            <input type="submit">
+        </form>
+        '''
+        error = self.request.get("error")
+        error_message = "<p class='error'>" + error + "</p>" if error else ""
+        content = header + form_body + error_message + footer
+
+        self.response.write(content)
+
+    def post(self):
+        username = self.request.get("username")
+        password = self.request.get("password")
+        verify = self.request.get("verify")
+        email = self.request.get("email")
+
+        # Check for valid username
+        if not username or not valid_username(username):
+            error = "Please enter a valid username."
+            self.redirect("/?error=" + error)
+        
+        # Check for valid password
+        elif (not password or not verify) or not valid_password(password) or password != verify:
+            error = "Please enter a valid password."
+            self.redirect("/?error=" + error)
+
+        # Check if email and if email is valid
+        elif email != "":
+            if not valid_email(email):
+                error = "Please use a valid email."
+                self.redirect("/?error=" + error)
+            else:
+                self.redirect("/welcome?username=" + username)    
+
+        # Redirect if all looks good
+        else: 
+            self.redirect("/welcome?username=" + username)
+
+class WelcomeHandler(webapp2.RequestHandler):        
+    def get(self):
+        username = self.request.get("username")
+        username = cgi.escape(username, quote=True)
+        self.response.write("<h1>Welcome " + username + "</h1>")
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/welcome', WelcomeHandler)
 ], debug=True)
